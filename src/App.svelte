86e1c710512e96044fe5de5a50c6d04f30b913a4
Michely <michelyparedes@gmail.com>
2023-10-20 13:42:22 +0200

    addition of main banner

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,24 @@
 
 <Header />
 
+<div class="welcome-panel">
+  <h1>Descubre el cine que lo cambia todo</h1>
+  <div class="subtitle">
+    <h2>
+      Con nuestra suscripción, accede a obras maestras , clásicos, cine
+      independiente, series exclusivas, documentales y mucho más.
+    </h2>
+  </div>
+  <p class="email-message">
+    Para iniciar tu suscripción, introduce tu email aquí
+  </p>
+  <input type="text" placeholder="Tu email" />
+  <button class="start-button">COMENZAR</button>
+  <p class="final-message">
+    Más de 10.000 películas y series. Cancela cuando quieras.
+  </p>
+</div>
+
 <p class="list-title">Nuestros estrenos exclusivos</p>
 <MovieList
   url="https://api.themoviedb.org/3/movie/now_playing?language=es&page=1"
@@ -30,5 +48,54 @@
     font-size: 25px;
     margin-left: 45px;
     margin-top: 80px;
+  }
+
+  .welcome-panel {
+    text-align: center;
+    height: 350px;
+    margin: 100px;
+  }
+
+  h1 {
+    font-size: 70px;
+    font-weight: bold;
+    color: white;
+  }
+
+  .subtitle {
+    width: 1000px;
+    margin: auto;
+    font-size: 20px;
+    color: white;
+  }
+
+  .email-message {
+    color: white;
+    margin-top: 50px;
+    font-size: 20px;
+  }
+
+  input {
+    width: 420px;
+    height: 35px;
+    background-color: #141426;
+    border-radius: 5px;
+    border-color: gray;
+    margin-right: 2px;
+    padding-left: 10px;
+    color: gray;
+  }
+  .start-button {
+    height: 40px;
+    width: 140px;
+    padding: 0 20px;
+    border-radius: 5px;
+    font-weight: bold;
+    background-color: #02ffa1;
+    border: none;
+  }
+
+  .final-message {
+    color: gray;
   }
 </style>
